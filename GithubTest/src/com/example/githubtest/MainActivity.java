00f40d3ba41416bacabb0543ee0f5ec809dc0dfc
Nicky_Liu <liufh8989@foxmail.com>
2015-11-29 21:31:04 +0800

    test2

--- GithubTest/src/com/example/githubtest/MainActivity.java
+++ GithubTest/src/com/example/githubtest/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i("","");
+        Log.i("","ttttt");
         setContentView(R.layout.activity_main);
     }
 }
